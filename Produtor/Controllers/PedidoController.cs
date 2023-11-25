4ab409b08136bb50a55f6663387fc0b24df5f05b
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-11-25 18:13:21 -0300

    Add project files.

---
+++ Produtor/Controllers/PedidoController.cs
@@ -0,0 +1,33 @@
+﻿using Core;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System.Formats.Asn1;
+
+namespace Produtor.Controllers
+{
+    [ApiController]
+    [Route("/Pedido")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private readonly IConfiguration _configuration;
+
+        public PedidoController(IBus bus, IConfiguration configuration)
+        {
+            _bus = bus;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var nomeFila = _configuration.GetSection("MassTransit")["NomeFila"] ?? string.Empty;
+            var endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{nomeFila}"));
+
+
+            await endpoint.Send(new Pedido(1, new Usuario(5,"Marcelo","marcelo@teste.com")));
+
+            return Ok();
+        }
+    }
+}
